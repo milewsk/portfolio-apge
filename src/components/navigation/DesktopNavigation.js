4866748c3ebb9606aa3ccbe4cbf8cc58f4d78232
Konrad Milewski <konradmilewski2000@interia.eu>
2022-04-10 11:19:25 +0200

    Styling desktop navigation

--- src/components/navigation/DesktopNavigation.js
+++ src/components/navigation/DesktopNavigation.js
@@ -1,10 +1,36 @@
+import { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../../sass/components/navigation/desktop-nav.scss'
+import my_logo from '../../img/logo/main-logo.png'
+import classNames from 'classnames'
+
 const DesktopNavigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>its desktop nav</li>
+    <header className="navigation-desktop">
+      <div className="navigation-desktop__logo-box">
+        <Link to="/">
+          <img
+            src={my_logo}
+            alt="Logo"
+            className="navigation-desktop__logo"
+          ></img>
+        </Link>
+      </div>
+      <ul className="navigation-desktop__list">
+        <li className="navigation-desktop__item">
+          <Link to="">Start</Link>
+        </li>
+        <li className="navigation-desktop__item">
+          <Link to="">About me</Link>
+        </li>
+        <li className="navigation-desktop__item">
+          <Link to="">Projects</Link>
+        </li>
+        <li className="navigation-desktop__item">
+          <Link to="">Contact me</Link>
+        </li>
       </ul>
-    </nav>
+    </header>
   )
 }
 
