838aae646fe912d61a9f4630814439fe9241a537
Konrad Milewski <konradmilewski2000@interia.eu>
2022-04-28 16:40:35 +0200

    HTML text, basic sass conf , animate home page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,11 +10,40 @@ import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Loader from 'react-loaders'
 import '../../sass/components/about/about.scss'
+import NavigationBar from '../navigation/NavigationBar'
 
 const About = () => {
   return (
     <Fragment>
-      <section className="section-about">Cześć tu kontakty</section>
+      <NavigationBar></NavigationBar>
+      <section className="section-about">
+        <h2 className="section-about__header">Know me better</h2>
+        <article className="section-about__article">
+          <h3 className="article__header">Who am I?</h3>
+          <p className="article__paragraf">
+            Hello my name is Konrad and I'm a computer science and econometrics
+            student at Bialystok University of Technology. I think, my friend
+            would describe me as an outgoing, friendly person with a lot of
+            creativity.
+          </p>
+          <h5 className="article__header--sub">My hobbies</h5>
+          <p>
+            Day by day I'm trying to improve myself at every possible filed. I
+            love sport, mostly going to the gym or playing basketball. I like to
+            read some mindopening books or watch a good film. Also, I in love
+            with creating new stuff like painting and obviously... programing.
+          </p>
+        </article>
+        <article className="section-about__article">
+          <h3 className="article__header">My technological stack</h3>
+          <p className="article__paragraf">
+            Mostly, I'm into creating web pages and making mockups. Slowly but
+            surely I'm trying to write more clean code and discover new usefull
+            features.
+          </p>
+          <h5 className="article__header--sub">Technologies</h5>
+        </article>
+      </section>
       <Loader type="cube-transition"></Loader>
     </Fragment>
   )
