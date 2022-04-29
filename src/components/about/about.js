5b41f0793f3c76448eb7c23848f4a9c7af0e3286
Konrad Milewski <konradmilewski2000@interia.eu>
2022-04-29 11:48:42 +0200

    Added list, styling headers and paragrafs

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -22,16 +22,17 @@ const About = () => {
           <h3 className="article__header">Who am I?</h3>
           <p className="article__paragraf">
             Hello my name is Konrad and I'm a computer science and econometrics
-            student at Bialystok University of Technology. I think, my friend
-            would describe me as an outgoing, friendly person with a lot of
-            creativity.
+            student at Bialystok University of Technology.<br></br> I think, my
+            friend would describe me as an outgoing, friendly person with a lot
+            of creativity.
           </p>
           <h5 className="article__header--sub">My hobbies</h5>
-          <p>
+          <p className="article__paragraf">
             Day by day I'm trying to improve myself at every possible filed. I
             love sport, mostly going to the gym or playing basketball. I like to
-            read some mindopening books or watch a good film. Also, I in love
-            with creating new stuff like painting and obviously... programing.
+            read some mindopening books or watch a good film.<br></br> Also, I
+            in love with creating new stuff like painting and obviously...
+            programing.
           </p>
         </article>
         <article className="section-about__article">
@@ -42,6 +43,18 @@ const About = () => {
             features.
           </p>
           <h5 className="article__header--sub">Technologies</h5>
+          <li className="article__list">
+            <ul className="list__item">HTML</ul>
+            <ul className="list__item">CSS</ul>
+            <ul className="list__item">Sass</ul>
+            <ul className="list__item">Javascript</ul>
+            <ul className="list__item">React</ul>
+            <ul className="list__item">
+              SOLID, DRY, RWD (at least I'm trying...)
+            </ul>
+            <ul className="list__item">GIT, Gitflow, Jira, Sourcetree</ul>
+            <ul className="list__item">C#, ORM, Linq, SQL</ul>
+          </li>
         </article>
       </section>
       <Loader type="cube-transition"></Loader>
